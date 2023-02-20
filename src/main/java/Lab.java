8f95348afaa3fa8ed6435441e1b3dbe16f35c2db
abdulkarim-hamidi <abdulkarim.hamidi@yahoo.com>
2023-02-20 21:28:57 +0000

    lab completed

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -1,32 +1,43 @@
 
+import java.util.HashMap;
 import java.util.Map;
 
 /**
- * Maps are key/value pairs. They are used in situations where we have some value that we'd like
- * to efficiently be able to access, but can be easily identified by some other value.
- * For instance, a dictionary 'key' may be the word and the 'value' may be its definition.
- * Or, if we'd like to keep track of how many times words were used in a book, we could use a word as a 'key'
+ * Maps are key/value pairs. They are used in situations where we have some 
+ value that we'd like
+ * to efficiently be able to access, but can be easily identified by some other
+ *  value.
+ * For instance, a dictionary 'key' may be the word and the 'value' may be its
+ *  definition.
+ * Or, if we'd like to keep track of how many times words were used in a book,
+ *  we could use a word as a 'key'
  * and the amount of times the word appeared as a 'value'.
  *
  * Keys may not be duplicates, but there may be duplicate values in a map.
  *
- * You can read more about HashMap, a very efficient implementation of Map, here:
+ * You can read more about HashMap, a very efficient implementation of Map, 
+ * here:
  * https://www.w3schools.com/java/java_hashmap.asp
  */
 public class Lab {
 
     /**
-     * Create and instantiate a collection that implements that Map interface, like
-     * TreeSet or HashSet. This will be a Map that maps Integer keys to String values.
+     * Create and instantiate a collection that implements that Map interface, 
+     * like
+     * TreeSet or HashSet. This will be a Map that maps Integer keys to String 
+     * values.
      * For example, 1:cat, 2:dog, 3:rat, etc.
      *
      * @return a new Map instantiated using Integer keys and String values.
-     * Like the other collections problems, Map alone is only an interface. Some common Map implementations are
-     * HashMap and TreeMap. Also like the other collections, Map must leverage a wrapper class instead of a primitive,
+     * Like the other collections problems, Map alone is only an interface.
+     *  Some common Map implementations are
+     * HashMap and TreeMap. Also like the other collections, Map must leverage
+     *  a wrapper class instead of a primitive,
      * so here Integer is a wrapper for int.
      */
     public Map<Integer, String> createMap(){
-        return null;
+        Map<Integer, String> createMap1 = new HashMap<>();
+        return createMap1;
     }
 
     /**
@@ -36,7 +47,8 @@ public class Lab {
      */
     public int getSize(Map<Integer,String> map){
 //        return total number of key/value pairs in the map
-        return 0;
+        // map = new HashMap<>();
+        return map.size();
     }
 
     /**
@@ -44,9 +56,12 @@ public class Lab {
      * @param map a map to be manipulated.
      * @param key the key of the new pair.
      * @param value the value of the new pair.
-     * @return nothing, pass by reference will cause changes to the list object to be reflected across the program.
+     * @return nothing, pass by reference will cause changes to the 
+     * list object to be reflected across the program.
      */
     public void addKeyValuePair(Map<Integer,String> map, int key, String value){
+        // map = new HashMap<>();
+        map.put(key, value);
     }
 
     /**
@@ -56,7 +71,8 @@ public class Lab {
      * @return the value associated with key in map.
      */
     public String getValueFromKey(Map<Integer, String> map, int key){
-        return "";
+        // map = new HashMap<>();
+        return map.get(key);
     }
 
     /**
@@ -66,6 +82,8 @@ public class Lab {
      * @return nothing, pass by reference will cause changes to the list object to be reflected across the program.
      */
     public void removeKeyValuePair(Map<Integer, String> map, int key){
+        // map = new HashMap<>();
+        map.remove(key);
     }
 
     /**
@@ -76,6 +94,8 @@ public class Lab {
      * @return nothing, pass by reference will cause changes to the list object to be reflected across the program.
      */
     public void overwriteValue(Map<Integer, String> map, int key, String value){
+        // map = new HashMap<>();
+        map.put(key, value);
 
     }
 }
